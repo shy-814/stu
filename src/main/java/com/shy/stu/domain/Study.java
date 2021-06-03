c0e6cead4518f9527262a050f412adba884e1859
songhanyong <shy@qq.com>
2021-06-04 00:10:11 +0800

    修改

---
+++ src/main/java/com/shy/stu/domain/Study.java
@@ -0,0 +1,13 @@
+package com.shy.stu.domain;
+
+/**
+ * @Author: songhanyong
+ * @Date: 2021-06-04 00:09
+ * @Since: 3.0.0.0
+ */
+public class Study {
+
+    public static void main(String[] args) {
+
+    }
+}
