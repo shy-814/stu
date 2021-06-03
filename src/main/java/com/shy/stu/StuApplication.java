5d475206092295811a263ef382e2feae212aa3d7
songhanyong <shy@qq.com>
2021-06-03 23:55:34 +0800

    创建git仓库

---
+++ src/main/java/com/shy/stu/StuApplication.java
@@ -0,0 +1,12 @@
+package com.shy.stu;
+
+/**
+ * @Author: songhanyong
+ * @Date: 2021-06-03 23:54
+ * @Since: 3.0.0.0
+ */
+public class StuApplication {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
